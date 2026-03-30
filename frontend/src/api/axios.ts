7f3da0644d4306e2316a9ab09fca8c33d1fcc120
“Leblanc” <nabongmichaeladriane@gmail.com>
2026-03-30 23:08:11 +0800

    axios setup

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
+    withXSRFToken: true,
+    headers: {
+        Accept: 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+    },
+})
